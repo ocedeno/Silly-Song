05b47a4b62d20d3e8e9df2b1534d8fa6ec0c7d99
Octavio Cedeno <ocedeno@Octavios-MacBook-Pro.local>
2017-02-23 10:26:18 -0500

    Completed App.

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,18 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    override func viewDidLoad() {
+    //IBOutlets:
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var lyricsTextBox: UITextView!
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        userNameTextField.delegate = self
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard)))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //IBActions:
+    @IBAction func createSong()
+    {
+        
+        guard (userNameTextField.text != nil) else
+        {
+            return
+        }
+        
+        lyricsTextBox.text = lyricsForName(lyricsTemplate: bananaFanaTemplate, fullName: userNameTextField.text!)
+        dismissKeyboard()
     }
+    
+    @IBAction func resetButton()
+    {
+        userNameTextField.text = ""
+        lyricsTextBox.text = ""
+    }
+    
+    func shortNameForName(name: String) -> String
+    {
+        let vowelCharacterSet = CharacterSet.init(charactersIn: "aeiouäöü")
+        guard let nameRange = name.lowercased().rangeOfCharacter(from: vowelCharacterSet) else {
+            print("There are no vowels.")
+            return name
+        }
+        let vowelIndex = nameRange.lowerBound
+        let range = name.substring(from: vowelIndex)
+        
+        return String(describing: range).lowercased()
+    }
+    
+    let bananaFanaTemplate =
+        [
+        "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+        "Banana Fana Fo F<SHORT_NAME>",
+        "Me My Mo M<SHORT_NAME>",
+        "<FULL_NAME>"
+        ].joined(separator: "\n")
 
-
+    func lyricsForName(lyricsTemplate: String, fullName: String) -> String
+    {
+        let shortName = shortNameForName(name: fullName)
+        let lyrics = lyricsTemplate
+            .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+            .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+        
+        return lyrics
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        createSong()
+        return false
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        resetButton()
+    }
+    
+    func dismissKeyboard()
+    {
+        userNameTextField.resignFirstResponder()
+    }
+    
 }
 
